29281de184d938e942bfd9f5cea1d189f966e736
Your Name <akshayrangasai.d@gmail.com>
2014-09-19 15:39:56 +0530

    Figure out what to do. Will code that shit into the model.

--- BuildBaysianModel.m
+++ BuildBaysianModel.m
@@ -35,17 +35,36 @@ classes = unique(trainData(:, end));
 model = cell(k, 2);
 class_matrices = zeros(m,n,k);
 
+%%For the combination of covariance matrices, we'll use the pooled
+%%covariance, mean and Wishart distribution estimates for the model.
+%%Whichever works the best. 
+
+
+if(caseNumber == 1)
+    C = cov(trainData(:,1:n));
+    for i = 1:k
+        model(k,2) = C;%cov(class_matrices(:,:,i);
+    end
+end
+    
+if(caseNumber == 2)
+    
 %Splitting the data into class matrices, so that we can find the mean and
 %variance for each class for the multinomial distribution.
-for i = 1:m
-    class_matrices(:,:,find(classes,trainData(i,-1)) = trainData(trainData(i,1:n);
-end
+    for i = 1:m
+        class_matrices(:,:,find(classes,trainData(i,-1)) = trainData(trainData(i,1:n);
+    end
 
 %cov_matrices = zeros(n,n,k);
 %mean_matrices = zeros(n,k);
 %determinants = zeros(k);
+    for i = 1:k
+        model(k,2) = cov(class_matrices(:,:,i);
+    end
+end
+
+
 for i = 1:k
-    model(k,2) = cov(class_matrices(:,:,i);
     model(k,1) = mean(class_matrices(:,:,i));
     %determinants(i) = det(cov_matrices(:,:,i));
 end

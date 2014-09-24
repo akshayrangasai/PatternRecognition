02df30e759770f59b690f662c3717712bd93c5ff
suthirth <suthirthv@gmail.com>
2014-09-25 00:26:06 +0530

    Decision boundaries fixed and report update

--- plots.m
+++ plots.m
@@ -1,17 +1,18 @@
 function plots(model, data)
 
 %decision boundary
-    W= zeros(2,2,2);
-    w = zeros(2,1,2);
+    W = cell(2,2);
+    w = cell(2,1);
     ww = zeros(2);
+    
     for i=1:2
-        W(i) = 0.5 * inv(cell2mat(model(i,2)));
-        w(i) = inv(cell2mat(model(i,2)))*cell2mat(model(i,1));
-        ww(i) = -0.5*cell2mat(model(i,1))'*inv(cell2mat(model(i,2))*cell2mat(model(i,1)) - 0.5*log(det(cell2mat(model(i,2))));
+        W(i) = mat2cell(0.5 * inv(cell2mat(model(i,2))));
+        w(i) = mat2cell(inv(cell2mat(model(i,2)))*cell2mat(model(i,1))');
+        ww(i) = -0.5*cell2mat(model(i,1))*inv(cell2mat(model(i,2)))*cell2mat(model(i,1))' - 0.5*log(det(cell2mat(model(i,2))));
     end
 
-    Wk = W(:,:,2) - W(:,:,1);
-    wk = w(:,:,2) - w(:,:,1);
+    Wk = cell2mat(W(2)) - cell2mat(W(1));
+    wk = cell2mat(w(2)) - cell2mat(w(1));
     wwk = ww(2) - ww(1);
 
     function z = decision(x,y,Wk,wk,wwk)

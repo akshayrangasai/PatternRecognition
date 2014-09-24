002b9f017f58471bfcf04ddc98d8aafb2bc420ec
suthirth <suthirthv@gmail.com>
2014-09-25 04:49:02 +0530

    decision boundaries done!

--- plots.m
+++ plots.m
@@ -3,11 +3,17 @@
     %load data
     load 'ls_group3.mat';
     trainData = ls_group3; 
-    set(gcf,'Name','Decision Boundaries');
+    xmin = min(trainData(:,1));
+    ymin = min(trainData(:,2));
+    xmax = max(trainData(:,1));
+    ymax = max(trainData(:,2));
+ 
+    set(gcf,'Name','Decision Boundaries'); 
     %Build models for all cases
-    for j=4:5
+    for j=1:5
     model = BuildBaysianModel(trainData, 0, j);
-    
+    mean_1 = cell2mat(model(1,1));
+    mean_2 = cell2mat(model(2,1));
  %%
     %Plot Decision Boundary
     W = cell(2,2);
@@ -15,7 +21,7 @@
     ww = zeros(2);
     
     for i=1:2
-        W(i) = mat2cell(0.5 * inv(cell2mat(model(i,2))));
+        W(i) = mat2cell(-0.5 * inv(cell2mat(model(i,2))));
         w(i) = mat2cell(inv(cell2mat(model(i,2)))*cell2mat(model(i,1))');
         ww(i) = -0.5*cell2mat(model(i,1))*inv(cell2mat(model(i,2)))*cell2mat(model(i,1))' - 0.5*log(det(cell2mat(model(i,2))));
     end
@@ -24,6 +30,18 @@
     wk = cell2mat(w(2)) - cell2mat(w(1));
     wwk = ww(2) - ww(1);
 
-    subplot(3,2,j); ezplot(@(x,y)decision(x,y,Wk,wk,wwk));
+    subplot(3,2,j); 
+    scatter(trainData(1:500,1),trainData(1:500,2),'red')
+    hold on
+    scatter(trainData(501:1000,1),trainData(501:1000,2),'green')
+    hold on
+%    scatter(trainData(1001:1500,1),trainData(1001:1500,2),'blue')
+%    hold on
+%    scatter(trainData(1501:2000,1),trainData(1501:2000,2),'black')
+%    hold on
+    ezplot(@(x,y)decision(x,y,Wk,wk,wwk),[xmin, xmax, ymin, ymax]);
+    setcurve ('color', 'black');
+    hold on
+    plot([mean_1(1,1) mean_2(1,1)], [mean_1(1,2) mean_2(1,2)],'yellow');
 
 end
---
+++ setcurve.m
@@ -0,0 +1,9 @@
+%change properties of last curve in current figure
+%Examples:
+%     setcurve('color','red')
+%     setcurve('color','green','linestyle','--')
+%Type  help plot  to see available colors and line styles 
+function setcurve(varargin)
+h=get(gca,'children');
+set(h(1),varargin{:})
+

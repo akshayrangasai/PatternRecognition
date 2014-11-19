45ffd4a9cc4fbba4c61a6362b0eb13466199f706
Your Name <akshayrangasai.d@gmail.com>
2014-11-19 16:38:51 +0530

    Wrote the data loader

--- FinalAssignment/parsedata.py
+++ FinalAssignment/parsedata.py
@@ -3,10 +3,12 @@ import numpy as np
 class dataset:
     def __init__(self, loc):
         self.directory = loc
+        self.datacontainer = []
     def loadData(self):
         for root, dirs, files in os.walk(self.directory):
             for f in files:
                 datapoint = np.genfromtxt(os.path.join(root,f))
+                self.datacontainer.append(datapoint)
 
     def aggregate(self):
         pass
